d52d07fcabbee710931ccd8f0a4425244c86ac4c
Dheeraj13540 <73415963+Dheeraj13540@users.noreply.github.com>
2022-01-24 10:39:24 +0530

    Update return biggest value x exactly x times in an array.py

--- return biggest value x exactly x times in an array.py
+++ return biggest value x exactly x times in an array.py
@@ -1,3 +1,16 @@
+1. Given A=(3, 8, 2, 3, 3, 21, the function should return 3. The value 2 occurs exactly two times and the value 3 occurs exactly three times, so they both meet the task conditions. The value 8 occurs just once, thus it does not meet the task conditions. The maximum of 2 and 3 is 3
+
+2. Given A-17, 1, 2, 8, 21, the function should return 2. The value 1 occurs exactly one time, the value 2 occurs exactly two times.
+
+3. Given A=(3, 1, 4, 1, 51, the function should return 0. There is no value
+
+which meets the task conditions,
+
+Test Output
+
+4 Given A 15, 5, 5, 5, 51, the function should return 5.
+
+            
 A=[3,8,2,3,3,2]
 A.sort()
 f=0
